1abb8b3e3a5c2cd0ba1cfd4c03496421ec40a474
Vu Khanh Linh <khanhlinh1252004@gmail.com>
2026-08-24 10:01:04 +0700

    Thêm router /register

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const Product = require("./models/Product");
+const bcrypt = require("bcryptjs");
+const User = require("./models/User");
 
 const app = express();
 app.use(cors());
@@ -72,6 +74,33 @@ app.delete("/products/:id", async function (req, res) {
   try {
     await Product.findByIdAndDelete(req.params.id);
     res.json({ message: "Đã xóa sản phẩm" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//route Đăng ký (/register)
+app.post("/register", async function (req, res) {
+  try {
+    const { name, email, password } = req.body; //destructuring, cách viết gọn để lấy nhiều giá trị từ 1 object cùng lúc, tương đương: const name = req.body.name;
+
+    // Kiểm tra email đã tồn tại chưa
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email đã được sử dụng" });
+    }
+
+    // Hash password trước khi lưu
+    const hashedPassword = await bcrypt.hash(password, 10); // Độ khó mã hoá(hash) = 10 càng cao càng an toàn nhưng càng chậm
+
+    const newUser = new User({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.json({ message: "Đăng ký thành công!" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
